78220256a3bcb6c7ca62d8c84d639677be6ead37
kubota yuki <ksw2170286@stu.o-hara.ac.jp>
2022-06-09 07:30:02 +0000

    入力促し結果を返す（未完成」

--- main.go
+++ main.go
@@ -12,9 +12,15 @@ func index(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+func result(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("result.html")
+	t.Execute(w, nil)
+}
+
 func main() {
 
 	http.HandleFunc("/", index)
+	http.HandleFunc("/result", result)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
