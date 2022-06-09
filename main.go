f38dbd9c5b4df99ea77383a887a64ab90a94aa03
kubota yuki <ksw2170286@stu.o-hara.ac.jp>
2022-06-09 07:20:52 +0000

    テンプレ作成

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
-func main() {
-	fmt.Println("サーバースタート([Ctrl]+[C]で終了")
+func index(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, nil)
+}
 
-	http.HandleFunc("/", handler)
+func main() {
+
+	http.HandleFunc("/", index)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -18,17 +22,5 @@ func main() {
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
-	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<html>\n<body>\n")
-	fmt.Fprintf(w, "<h1>httpserverへようこそ</h1>\n")
-	fmt.Fprintf(w, "<p>サーバーは:%q</p>\n", r.Host)
-	fmt.Fprintf(w, "<p>リモートアドレスは:%q</p>\n", r.RemoteAddr)
-	fmt.Fprintf(w, "</body>\n</html>\n")
-
-	if err := r.ParseForm(); err != nil {
-		log.Print(err)
 	}
 }
